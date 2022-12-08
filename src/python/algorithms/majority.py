452ef2935be6a3e080c62fa085047e4db9902575
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-12-08 12:40:33 -0300

    refatoring majority algorithms

--- src/python/algorithms/majority.py
+++ src/python/algorithms/majority.py
@@ -1,9 +1,28 @@
-def majority(vector, min, max):
-    q = 0
-    for i in range(max):
-        if vector[i] == vector[min]:
-            q = q + 1
-    if q > (max // 2):
-        return vector[min]
-    else:
-        return majority(vector, min + 1, max)
+def findMajorityElement(vector, begin, end):
+    # Caso base
+    if begin == end:
+        return vector[begin]
+
+    # Divisão
+    middle = (begin + end)//2
+    left = findMajorityElement(vector, begin, middle)
+    right = findMajorityElement(vector, middle + 1, end)
+
+    # Conquista
+    if left == right:
+        return left
+    if vector.count(left) > middle:
+        return left
+    if vector.count(right) > middle:
+        return right
+
+
+def main():
+    vector = [4, 4, 2, 4, 7, 6, 4, 2, 4, 4, 7]
+    print('Input:', vector)
+    print('The length of vector:', len(vector))
+    result = findMajorityElement(vector, 0, len(vector) - 1)
+    print('The most frequency element:', result)
+
+
+main()
